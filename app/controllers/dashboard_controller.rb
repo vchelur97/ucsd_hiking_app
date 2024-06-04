b1a923690b219388f424b2832aae9215c7af47b3
Vineeth Chelur <vchelur@ucsd.edu>
2024-06-04 02:07:50 +0000

    Allow drafting and reviewing hikes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   def index
+    @draft_hikes = Hike.where(status: 'draft', host: user).order(date: :asc)
+    @review_hikes = Hike.where(status: 'review').order(date: :asc) if user.admin?
     @upcoming_hikes = Hike.where(status: 'published').where('date >= ?', Date.today).order(date: :asc)
   end
 end
